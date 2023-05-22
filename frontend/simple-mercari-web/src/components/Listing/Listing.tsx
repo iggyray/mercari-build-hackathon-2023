0df5dd8bbbf26fd16453bf8a049c40f6fd0f58a9
iggy <iggynelo@gmail.com>
2023-05-23 00:01:48 +0900

    installed bootstrap dependency and styled listing page to test css works

--- frontend/simple-mercari-web/src/components/Listing/Listing.tsx
+++ frontend/simple-mercari-web/src/components/Listing/Listing.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
-import { MerComponent } from "../MerComponent";
-import { toast } from "react-toastify";
-import { fetcher } from "../../helper";
+import React, { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
+import { MerComponent } from "../MerComponent"
+import { toast } from "react-toastify"
+import { fetcher } from "../../helper"
 
 interface Category {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
 
 type formDataType = {
-  name: string;
-  category_id: number;
-  price: number;
-  description: string;
-  image: string | File;
-};
+  name: string
+  category_id: number
+  price: number
+  description: string
+  image: string | File
+}
 
 export const Listing: React.FC = () => {
   const initialState = {
@@ -24,40 +24,40 @@ export const Listing: React.FC = () => {
     price: 0,
     description: "",
     image: "",
-  };
-  const [values, setValues] = useState<formDataType>(initialState);
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [cookies] = useCookies(["token", "userID"]);
+  }
+  const [values, setValues] = useState<formDataType>(initialState)
+  const [categories, setCategories] = useState<Category[]>([])
+  const [cookies] = useCookies(["token", "userID"])
 
   const onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({
       ...values,
       [event.target.name]: event.target.value,
-    });
-  };
+    })
+  }
 
   const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setValues({
       ...values,
       [event.target.name]: event.target.value,
-    });
-  };
+    })
+  }
 
   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({
       ...values,
       [event.target.name]: event.target.files![0],
-    });
-  };
+    })
+  }
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData();
-    data.append("name", values.name);
-    data.append("category_id", values.category_id.toString());
-    data.append("price", values.price.toString());
-    data.append("description", values.description);
-    data.append("image", values.image);
+    event.preventDefault()
+    const data = new FormData()
+    data.append("name", values.name)
+    data.append("category_id", values.category_id.toString())
+    data.append("price", values.price.toString())
+    data.append("description", values.description)
+    data.append("image", values.image)
 
     fetcher<{ id: number }>(`/items`, {
       method: "POST",
@@ -67,13 +67,13 @@ export const Listing: React.FC = () => {
       },
     })
       .then((res) => {
-        sell(res.id);
+        sell(res.id)
       })
       .catch((error: Error) => {
-        toast.error(error.message);
-        console.error("POST error:", error);
-      });
-  };
+        toast.error(error.message)
+        console.error("POST error:", error)
+      })
+  }
 
   const sell = (itemID: number) =>
     fetcher(`/sell`, {
@@ -88,12 +88,12 @@ export const Listing: React.FC = () => {
       }),
     })
       .then((_) => {
-        toast.success("Item added successfully!");
+        toast.success("Item added successfully!")
       })
       .catch((error: Error) => {
-        toast.error(error.message);
-        console.error("POST error:", error);
-      });
+        toast.error(error.message)
+        console.error("POST error:", error)
+      })
 
   const fetchCategories = () => {
     fetcher<Category[]>(`/items/categories`, {
@@ -105,68 +105,83 @@ export const Listing: React.FC = () => {
     })
       .then((items) => setCategories(items))
       .catch((err) => {
-        console.log(`GET error:`, err);
-        toast.error(err.message);
-      });
-  };
+        console.log(`GET error:`, err)
+        toast.error(err.message)
+      })
+  }
 
   useEffect(() => {
-    fetchCategories();
-  }, []);
+    fetchCategories()
+  }, [])
 
   return (
     <MerComponent>
       <div className="Listing">
         <form onSubmit={onSubmit} className="ListingForm">
           <div>
-            <input
-              type="text"
-              name="name"
-              id="MerTextInput"
-              placeholder="name"
-              onChange={onValueChange}
-              required
-            />
-            <select
-              name="category_id"
-              id="MerTextInput"
-              value={values.category_id}
-              onChange={onSelectChange}
-            >
-              {categories &&
-                categories.map((category) => {
-                  return <option value={category.id}>{category.name}</option>;
-                })}
-            </select>
-            <input
-              type="number"
-              name="price"
-              id="MerTextInput"
-              placeholder="price"
-              onChange={onValueChange}
-              required
-            />
-            <input
-              type="text"
-              name="description"
-              id="MerTextInput"
-              placeholder="description"
-              onChange={onValueChange}
-              required
-            />
-            <input
-              type="file"
-              name="image"
-              id="MerTextInput"
-              onChange={onFileChange}
-              required
-            />
-            <button type="submit" id="MerButton">
+            <div className="mb-3">
+              <input
+                type="text"
+                name="name"
+                id="MerTextInput"
+                className="form-control"
+                placeholder="name"
+                onChange={onValueChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <select
+                name="category_id"
+                id="MerTextInput"
+                className="form-select"
+                value={values.category_id}
+                onChange={onSelectChange}
+              >
+                {categories &&
+                  categories.map((category) => {
+                    return <option value={category.id}>{category.name}</option>
+                  })}
+              </select>
+            </div>
+            <div className="mb-3">
+              <input
+                type="number"
+                name="price"
+                id="MerTextInput"
+                className="form-control"
+                placeholder="price"
+                onChange={onValueChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                name="description"
+                id="MerTextInput"
+                className="form-control"
+                placeholder="description"
+                onChange={onValueChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="file"
+                name="image"
+                id="MerTextInput"
+                className="form-control"
+                onChange={onFileChange}
+                required
+              />
+            </div>
+            <button type="submit" id="MerButton" className="btn btn-danger">
               List this item
             </button>
           </div>
         </form>
       </div>
     </MerComponent>
-  );
-};
+  )
+}
