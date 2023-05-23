bb096f161b75b2298959d23407137257225fad22
iggy <iggynelo@gmail.com>
2023-05-24 00:09:26 +0900

    simplify listed items on main page to only display price and name

--- frontend/simple-mercari-web/src/components/Item/Item.tsx
+++ frontend/simple-mercari-web/src/components/Item/Item.tsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
-import { fetcherBlob } from "../../helper";
+import { useState, useEffect } from "react"
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom"
+import { fetcherBlob } from "../../helper"
 
 interface Item {
-  id: number;
-  name: string;
-  price: number;
-  category_name: string;
+  id: number
+  name: string
+  price: number
+  category_name: string
 }
 
 export const Item: React.FC<{ item: Item }> = ({ item }) => {
-  const navigate = useNavigate();
-  const [itemImage, setItemImage] = useState<string>("");
-  const [cookies] = useCookies(["token"]);
+  const navigate = useNavigate()
+  const [itemImage, setItemImage] = useState<string>("")
+  const [cookies] = useCookies(["token"])
 
   async function getItemImage(itemId: number): Promise<Blob> {
     return await fetcherBlob(`/items/${itemId}/image`, {
@@ -23,34 +23,27 @@ export const Item: React.FC<{ item: Item }> = ({ item }) => {
         Accept: "application/json",
         Authorization: `Bearer ${cookies.token}`,
       },
-    });
+    })
   }
 
   useEffect(() => {
     async function fetchData() {
-      const image = await getItemImage(item.id);
-      setItemImage(URL.createObjectURL(image));
+      const image = await getItemImage(item.id)
+      setItemImage(URL.createObjectURL(image))
     }
 
-    fetchData();
-  }, [item]);
+    fetchData()
+  }, [item])
 
   return (
-    <div>
-      <h3>{item.name}</h3>
-      <img
-        src={itemImage}
-        alt={item.name}
-        height={480}
-        width={480}
-        onClick={() => navigate(`/item/${item.id}`)}
-      />
-      <p>
-        <span>Category: {item.category_name}</span>
-        <br />
-        <span>Price: {item.price}</span>
-        <br />
-      </p>
+    <div className="ItemContainer" onClick={() => navigate(`/item/${item.id}`)}>
+      <div className="ImgContainer">
+        <img src={itemImage} alt={item.name} />
+        <div className="PriceTag">
+          <p className="h6"> Price: {item.price}</p>
+        </div>
+      </div>
+      <p className="h5">{item.name}</p>
     </div>
-  );
-};
+  )
+}
