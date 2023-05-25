683fb134aee065416bf86e9310cb490664b32afc
iggy <iggynelo@gmail.com>
2023-05-26 00:08:51 +0900

    added some simple styles to item details page

--- frontend/simple-mercari-web/src/components/ItemDetail/ItemDetail.tsx
+++ frontend/simple-mercari-web/src/components/ItemDetail/ItemDetail.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import { MerComponent } from "../MerComponent";
-import { toast } from "react-toastify";
-import { fetcher, fetcherBlob } from "../../helper";
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { useCookies } from "react-cookie"
+import { MerComponent } from "../MerComponent"
+import { toast } from "react-toastify"
+import { fetcher, fetcherBlob } from "../../helper"
 
 const ItemStatus = {
   ItemStatusInitial: 1,
   ItemStatusOnSale: 2,
   ItemStatusSoldOut: 3,
-} as const;
+} as const
 
-type ItemStatus = typeof ItemStatus[keyof typeof ItemStatus];
+type ItemStatus = (typeof ItemStatus)[keyof typeof ItemStatus]
 
 interface Item {
-  id: number;
-  name: string;
-  category_id: number;
-  category_name: string;
-  user_id: number;
-  price: number;
-  status: ItemStatus;
-  description: string;
+  id: number
+  name: string
+  category_id: number
+  category_name: string
+  user_id: number
+  price: number
+  status: ItemStatus
+  description: string
 }
 
 export const ItemDetail = () => {
-  const navigate = useNavigate();
-  const params = useParams();
-  const [item, setItem] = useState<Item>();
-  const [itemImage, setItemImage] = useState<Blob>();
-  const [cookies] = useCookies(["token", "userID"]);
+  const navigate = useNavigate()
+  const params = useParams()
+  const [item, setItem] = useState<Item>()
+  const [itemImage, setItemImage] = useState<Blob>()
+  const [cookies] = useCookies(["token", "userID"])
 
   const fetchItem = () => {
     fetcher<Item>(`/items/${params.id}`, {
@@ -40,13 +40,13 @@ export const ItemDetail = () => {
       },
     })
       .then((res) => {
-        console.log("GET success:", res);
-        setItem(res);
+        console.log("GET success:", res)
+        setItem(res)
       })
       .catch((err) => {
-        console.log(`GET error:`, err);
-        toast.error(err.message);
-      });
+        console.log(`GET error:`, err)
+        toast.error(err.message)
+      })
 
     fetcherBlob(`/items/${params.id}/image`, {
       method: "GET",
@@ -56,14 +56,14 @@ export const ItemDetail = () => {
       },
     })
       .then((res) => {
-        console.log("GET success:", res);
-        setItemImage(res);
+        console.log("GET success:", res)
+        setItemImage(res)
       })
       .catch((err) => {
-        console.log(`GET error:`, err);
-        toast.error(err.message);
-      });
-  };
+        console.log(`GET error:`, err)
+        toast.error(err.message)
+      })
+  }
 
   const onSubmit = (_: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if (cookies.token && cookies.userID) {
@@ -80,53 +80,51 @@ export const ItemDetail = () => {
       })
         .then((_) => window.location.reload())
         .catch((err) => {
-          console.log(`POST error:`, err);
-          toast.error(err.message);
-        });
+          console.log(`POST error:`, err)
+          toast.error(err.message)
+        })
     } else {
       navigate("/login")
     }
-  };
+  }
 
   useEffect(() => {
-    fetchItem();
-  }, []);
+    fetchItem()
+  }, [])
 
   return (
     <div className="ItemDetail">
       <MerComponent condition={() => item !== undefined}>
         {item && itemImage && (
-          <div>
+          <div className="ItemDetailWrapper">
             <img
-              height={480}
-              width={480}
               src={URL.createObjectURL(itemImage)}
               alt="item"
               onClick={() => navigate(`/item/${item.id}`)}
             />
-            <p>
-              <span>Item Name: {item.name}</span>
-              <br />
-              <span>Price: {item.price}</span>
-              <br />
-              <span>UserID: {item.user_id}</span>
-              <br />
-              <span>Category: {item.category_name}</span>
-              <br />
-              <span>Description: {item.description}</span>
-            </p>
-            {item.status == ItemStatus.ItemStatusSoldOut ? (
-              <button disabled={true} onClick={onSubmit} id="MerDisableButton">
-                SoldOut
-              </button>
-            ) : (
-              <button onClick={onSubmit} id="MerButton">
-                Purchase
-              </button>
-            )}
+            <div className="ItemDetailDesc">
+              <h1 className="display-2">{item.name}</h1>
+              <h4 className="ItemCategory">{item.category_name}</h4>
+              <h3>¥ {item.price}</h3>
+              <h4>Description:</h4>
+              <p>{item.description}</p>
+              {item.status == ItemStatus.ItemStatusSoldOut ? (
+                <button
+                  disabled={true}
+                  onClick={onSubmit}
+                  id="MerDisableButton"
+                >
+                  SoldOut
+                </button>
+              ) : (
+                <button onClick={onSubmit} id="MerButton">
+                  Purchase
+                </button>
+              )}
+            </div>
           </div>
         )}
       </MerComponent>
     </div>
-  );
-};
+  )
+}
