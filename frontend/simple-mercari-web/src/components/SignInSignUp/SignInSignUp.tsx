f01772b40c29f7bac1d68c834d7799741ab927ed
tonklable <tonklalor2544@gmail.com>
2023-05-25 17:44:49 +0900

    Redirect login page for login user
    - Redirect localhost:3000/login to user Account page if user is already login

--- frontend/simple-mercari-web/src/components/SignInSignUp/SignInSignUp.tsx
+++ frontend/simple-mercari-web/src/components/SignInSignUp/SignInSignUp.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Login } from '../Login/Login'
 import { Signup } from '../Signup/Signup'
-
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom";
 
 export const SignUpAndSignIn: React.FC = () => {
     const [isLogInPage, setIsLogInPage] = useState(true)
-
+    const [cookies] = useCookies(["userID", "token"])
+    const navigate = useNavigate();
     const toggleIsLogInPage = () => {
         setIsLogInPage(!isLogInPage)
     }
+    useEffect(() => {
+
+
+        if (cookies.token ||
+            cookies.userID) {
+            navigate(`/user/${cookies.userID}`)
+        }
+
+    }, [cookies, navigate])
+
     return (
         <>
             {!isLogInPage && (
