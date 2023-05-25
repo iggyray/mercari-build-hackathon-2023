b928c6dec647fc61d4ebb93ece1a0739b839cdc2
iggy <iggynelo@gmail.com>
2023-05-25 14:45:58 +0900

    debug search function

--- frontend/simple-mercari-web/src/App.tsx
+++ frontend/simple-mercari-web/src/App.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Home } from "./components/Home";
-import { ItemDetail } from "./components/ItemDetail";
-import { UserProfile } from "./components/UserProfile";
-import { Listing } from "./components/Listing";
-import "./App.css";
-import { Header } from "./components/Header/Header";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Home } from "./components/Home"
+import { ItemDetail } from "./components/ItemDetail"
+import { UserProfile } from "./components/UserProfile"
+import { Listing } from "./components/Listing"
+import "./App.css"
+import { Header } from "./components/Header/Header"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-export const App: React.VFC = () => {
+export const App: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>("")
+  const handleSearch = (value: string) => {
+    setSearchValue(value)
+  }
   return (
     <>
       <ToastContainer position="bottom-center" />
 
       <BrowserRouter>
         <div className="MerComponent">
-          <Header></Header>
+          <Header onSearch={handleSearch}></Header>
           <Routes>
-            <Route index element={<Home />} />
+            <Route index element={<Home searchValue={searchValue} />} />
             <Route path="/item/:id" element={<ItemDetail />} />
             <Route path="/user/:id" element={<UserProfile />} />
             <Route path="/sell" element={<Listing />} />
@@ -26,5 +30,5 @@ export const App: React.VFC = () => {
         </div>
       </BrowserRouter>
     </>
-  );
-};
+  )
+}
