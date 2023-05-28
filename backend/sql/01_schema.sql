cf9ef5b7025d0309e2cc97b3df13b2a513585e19
iggy <iggynelo@gmail.com>
2023-05-28 14:05:55 +0900

    extended comments table to include parent comment id

--- backend/sql/01_schema.sql
+++ backend/sql/01_schema.sql
@@ -35,11 +35,13 @@ CREATE TABLE IF NOT EXISTS status
 CREATE TABLE IF NOT EXISTS comments
 (
     comment_id integer primary key autoincrement,
+    parent_comment_id integer,
     user_id integer NOT NULL,
     user_name varchar(50) NOT NULL,
     item_id integer NOT NULL,
     content text NOT NULL,
     created_at text NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    foreign key (parent_comment_id) references comments (comment_id),
     foreign key (user_id) references users (id),
     foreign key (item_id) references items (id)
 );
